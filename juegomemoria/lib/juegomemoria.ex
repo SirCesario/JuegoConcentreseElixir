b799eab6e23ca9872e719edb3cde13b3d3f0107c
Julio Cesar Cuevas <jahve20@hotmail.com>
2023-03-10 22:15:37 -0500

    Generando proyecto y cambios

---
+++ juegomemoria/lib/juegomemoria.ex
@@ -0,0 +1,18 @@
+defmodule Juegomemoria do
+  @moduledoc """
+  Documentation for `Juegomemoria`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Juegomemoria.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ juegomemoria/test/juegomemoria_test.exs
@@ -0,0 +1,8 @@
+defmodule JuegomemoriaTest do
+  use ExUnit.Case
+  doctest Juegomemoria
+
+  test "greets the world" do
+    assert Juegomemoria.hello() == :world
+  end
+end
